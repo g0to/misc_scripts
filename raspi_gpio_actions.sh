4a77f45249b4c6ce161b3b09f16253d2bf87fa9f
g0to <amoral@gmail.com>
2013-05-13 18:51:26 +0200

    Fix a little mess with tab spaces in the file

--- raspi_gpio_actions.sh
+++ raspi_gpio_actions.sh
@@ -11,35 +11,35 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 #                                                                             #
 # -Raspberry Pi Rev2. GPIO layout:                                            #
-#                                               			      #
-#           2  4  6  8  10 12 14 16 18 20 22 24 26    _R_C_A_		      #
-#           |  |  |  |  |  |  |  |  |  |  |  |  |     |______|  	      #
-#           --------------------------------------    		              #
-#           |  |  |  |  |  |  |  |  |  |  |  |  |			      #
-#    _      1  3  5  7  9  11 13 15 17 19 21 23 25			      #
-#   | |									      #
-#   | | SD Card								      #
-#   | |									      #
-#   |_|									      #
-#									      #
-#   P1-01: 3.3v       P1-10: GPIO15     P1-20: GND			      #
-#   P1-02: 5.0v       P1-11: GPIO17     P1-21: GPIO9			      #
-#   P1-03: GPIO2      P1-12: GPIO18     P1-22: GPIO25			      #
-#   P1-04: 5.0v       P1-13: GPIO27     P1-23: GPIO11			      #
-#   P1-05: GPIO3      P1-14: GND        P1-24: GPIO8			      #
-#   P1-06: GND        P1-15: GPIO22     P1-25: GND			      #
-#   P1-07: GPIO4      P1-16: GPIO23     P1-26: GPIO7   			      #
-#   P1-08: GPIO14     P1-17: 3.3v    					      #
-#   P1-09: GND        P1-18: GPIO24 					      #
-#                     P1-19: GPIO10					      #
+#                                               			                  #
+#           2  4  6  8  10 12 14 16 18 20 22 24 26    _R_C_A_		          #
+#           |  |  |  |  |  |  |  |  |  |  |  |  |     |______|  	          #
+#           --------------------------------------    		                  #
+#           |  |  |  |  |  |  |  |  |  |  |  |  |			                  #
+#    _      1  3  5  7  9  11 13 15 17 19 21 23 25			                  #
+#   | |									                                      #
+#   | | SD Card								                                  #
+#   | |									                                      #
+#   |_|									                                      #
+#									                                          #
+#   P1-01: 3.3v       P1-10: GPIO15     P1-20: GND			                  #
+#   P1-02: 5.0v       P1-11: GPIO17     P1-21: GPIO9			              #
+#   P1-03: GPIO2      P1-12: GPIO18     P1-22: GPIO25			              #
+#   P1-04: 5.0v       P1-13: GPIO27     P1-23: GPIO11			              #
+#   P1-05: GPIO3      P1-14: GND        P1-24: GPIO8			              #
+#   P1-06: GND        P1-15: GPIO22     P1-25: GND			                  #
+#   P1-07: GPIO4      P1-16: GPIO23     P1-26: GPIO7   			              #
+#   P1-08: GPIO14     P1-17: 3.3v    					                      #
+#   P1-09: GND        P1-18: GPIO24 					                      #
+#                     P1-19: GPIO10					                          #
 #                                                                             #
 # NOTE: Pins GPIO2 and GPIO3 have a 1K8 pull up resistor, which means that    # 
 # you can short cirtuit them directly to ground and you'll get a 1 as a non   #
 # connected value (iddle) and a 0 as a connected value (short circuit to GND) #
-# when reading them.           						      #
+# when reading them.           						                          #
 #                                                                             #
 #                                                                             #
-#  by g0to								      #
+#  by g0to								                                      #
 #                                                                             #
  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #  
 
